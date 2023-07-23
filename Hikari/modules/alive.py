0610eb7a32a08a635db4c3c36c6c7b58209a74a6
pinxRobtik <rezastyle69@gmail.com>
2023-07-23 15:29:42 +0700

    initial

---
+++ Hikari/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from Hikari.events import register
+from Hikari import telethn as tbot
+
+
+PHOTO = "https://telegra.ph//file/b413cf48b78b3210b0a2d.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), Aᴋᴜ ᴇɴᴠᴏʟᴇʀ ʀᴏʙᴏᴛ .** \n\n"
+  TEXT += "๏ **ᴀᴋᴜ sᴇʟᴀʟᴜ ʜɪᴅᴜᴘ ᴅᴀɴ ʙᴇᴋᴇʀᴊᴀ** \n\n"
+  TEXT += f"๏ **ᴍʏ ᴏᴡɴᴇʀ : [Dexz](https://t.me/Dexzishere)** \n\n"
+  TEXT += f"๏ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+  TEXT += f"๏ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+  TEXT += f"๏ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
+  TEXT += "**ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴋᴜ ᴅɪsɪɴɪ 🔥**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/punyaenvolernihbot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/bieinhere")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+<img src="https://telegra.ph//file/474351d74344a8402848d.jpg">
+
+</p>
+
+
+
+
+# credits
+- [pinxRobtik](https://github.com/pinxrobtik)
